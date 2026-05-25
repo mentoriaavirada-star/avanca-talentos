5910ae159591042ef9e36fda3bfec511ccfbe58d
mentoriaavirada-star <mentoriaavirada@gmail.com>
2026-05-24 22:38:39 -0300

    Delete src/app/src/app/page.tsx

--- src/app/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-import { auth } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
-
-export default function Home() {
-  const { userId } = auth();
-  if (userId) redirect("/dashboard");
-  else redirect("/sign-in");
-}
