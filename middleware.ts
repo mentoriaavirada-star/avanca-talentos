928f358ed0ec9f0524f0f14b45d12b0d06270216
mentoriaavirada-star <mentoriaavirada@gmail.com>
2026-05-25 00:03:53 -0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,7 @@ const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/pdi/(.*)"]);
 
 export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    const authObj = await auth();
-    authObj.protect();
+    await auth.protect();
   }
 });
 
