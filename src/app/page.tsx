4373f3cdfb933d9372d7834c7f9736d9c0560346
mentoriaavirada-star <mentoriaavirada@gmail.com>
2026-07-08 10:09:27 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
-  const { userId } = await auth();
-  if (userId) redirect("/dashboard");
-  else redirect("/sign-in");
+  redirect("/dashboard");
 }
